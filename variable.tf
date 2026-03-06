2ff3c6543fdfe493f606d3fc004fc89cfe381dc5
thejagowd <thejagowdvn@gmail.com>
2026-03-06 13:19:40 +0530

    Update variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "webapp-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "app_service_plan_name" {
+  default = "webapp-plan"
+}
+
+variable "web_app_name" {
+  default = "myterraformwebapp123"
+}
